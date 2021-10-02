1fe3888686a9311126b17f5ffb2dc9660ef87d4b
Cheng Huang <huangchengadam@gmail.com>
2021-10-02 21:34:09 +0200

    pie chart & bubble chart & line chart

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+import numpy as np
 import pandas as pd
 import plotly.express as px  # (version 4.7.0)
 import dash
@@ -8,30 +12,158 @@ import plotly.graph_objects as go
 
 
 app = dash.Dash(__name__)
-
+all_country_option = ['Finland', 'Denmark', 'Switzerland', 'Iceland', 'Netherlands', 'Norway', 'Sweden', 'Luxembourg', 'New Zealand', 'Austria', 'Australia', 'Israel', 'Germany', 'Canada', 'Ireland', 'Costa Rica', 'United Kingdom', 'Czech Republic', 'United States', 'Belgium', 'France', 'Bahrain', 'Malta', 'Taiwan Province of China', 'United Arab Emirates', 'Saudi Arabia', 'Spain', 'Italy', 'Slovenia', 'Guatemala', 'Uruguay', 'Singapore', 'Kosovo', 'Slovakia', 'Brazil', 'Mexico', 'Jamaica', 'Lithuania', 'Cyprus', 'Estonia', 'Panama', 'Uzbekistan', 'Chile', 'Poland', 'Kazakhstan', 'Romania', 'Kuwait', 'Serbia', 'El Salvador', 'Mauritius', 'Latvia', 'Colombia', 'Hungary', 'Thailand', 'Nicaragua', 'Japan', 'Argentina', 'Portugal', 'Honduras', 'Croatia', 'Philippines', 'South Korea', 'Peru', 'Bosnia and Herzegovina', 'Moldova', 'Ecuador', 'Kyrgyzstan', 'Greece', 'Bolivia', 'Mongolia', 'Paraguay', 'Montenegro', 'Dominican Republic', 'North Cyprus', 'Belarus', 'Russia', 'Hong Kong S.A.R. of China', 'Tajikistan', 'Vietnam', 'Libya', 'Malaysia', 'Indonesia', 'Congo (Brazzaville)', 'China', 'Ivory Coast', 'Armenia', 'Nepal', 'Bulgaria', 'Maldives', 'Azerbaijan', 'Cameroon', 'Senegal', 'Albania', 'North Macedonia', 'Ghana', 'Niger', 'Turkmenistan', 'Gambia', 'Benin', 'Laos', 'Bangladesh', 'Guinea', 'South Africa', 'Turkey', 'Pakistan', 'Morocco', 'Venezuela', 'Georgia', 'Algeria', 'Ukraine', 'Iraq', 'Gabon', 'Burkina Faso', 'Cambodia', 'Mozambique', 'Nigeria', 'Mali', 'Iran', 'Uganda', 'Liberia', 'Kenya', 'Tunisia', 'Lebanon', 'Namibia', 'Palestinian Territories', 'Myanmar', 'Jordan', 'Chad', 'Sri Lanka', 'Swaziland', 'Comoros', 'Egypt', 'Ethiopia', 'Mauritania', 'Madagascar', 'Togo', 'Zambia', 'Sierra Leone', 'India', 'Burundi', 'Yemen', 'Tanzania', 'Haiti', 'Malawi', 'Lesotho', 'Botswana', 'Rwanda', 'Zimbabwe', 'Afghanistan', 'Finland', 'Denmark', 'Switzerland', 'Iceland', 'Netherlands', 'Norway', 'Sweden', 'Luxembourg', 'New Zealand', 'Austria', 'Australia', 'Israel', 'Germany', 'Canada', 'Ireland', 'Costa Rica', 'United Kingdom', 'Czech Republic', 'United States', 'Belgium', 'France', 'Bahrain', 'Malta', 'Taiwan Province of China', 'United Arab Emirates', 'Saudi Arabia', 'Spain', 'Italy', 'Slovenia', 'Guatemala', 'Uruguay', 'Singapore', 'Kosovo', 'Slovakia', 'Brazil', 'Mexico', 'Jamaica', 'Lithuania', 'Cyprus', 'Estonia', 'Panama', 'Uzbekistan', 'Chile', 'Poland', 'Kazakhstan', 'Romania', 'Kuwait', 'Serbia', 'El Salvador', 'Mauritius', 'Latvia', 'Colombia', 'Hungary', 'Thailand', 'Nicaragua', 'Japan', 'Argentina', 'Portugal', 'Honduras', 'Croatia', 'Philippines', 'South Korea', 'Peru', 'Bosnia and Herzegovina', 'Moldova', 'Ecuador', 'Kyrgyzstan', 'Greece', 'Bolivia', 'Mongolia', 'Paraguay', 'Montenegro', 'Dominican Republic', 'North Cyprus', 'Belarus', 'Russia', 'Hong Kong S.A.R. of China', 'Tajikistan', 'Vietnam', 'Libya', 'Malaysia', 'Indonesia', 'Congo (Brazzaville)', 'China', 'Ivory Coast', 'Armenia', 'Nepal', 'Bulgaria', 'Maldives', 'Azerbaijan', 'Cameroon', 'Senegal', 'Albania', 'North Macedonia', 'Ghana', 'Niger', 'Turkmenistan', 'Gambia', 'Benin', 'Laos', 'Bangladesh', 'Guinea', 'South Africa', 'Turkey', 'Pakistan', 'Morocco', 'Venezuela', 'Georgia', 'Algeria', 'Ukraine', 'Iraq', 'Gabon', 'Burkina Faso', 'Cambodia', 'Mozambique', 'Nigeria', 'Mali', 'Iran', 'Uganda', 'Liberia', 'Kenya', 'Tunisia', 'Lebanon', 'Namibia', 'Palestinian Territories', 'Myanmar', 'Jordan', 'Chad', 'Sri Lanka', 'Swaziland', 'Comoros', 'Egypt', 'Ethiopia', 'Mauritania', 'Madagascar', 'Togo', 'Zambia', 'Sierra Leone', 'India', 'Burundi', 'Yemen', 'Tanzania', 'Haiti', 'Malawi', 'Lesotho', 'Botswana', 'Rwanda', 'Zimbabwe', 'Afghanistan']
 app.layout = html.Div([
-    html.H1("World Happiness Report Data Visulization", style={'text-align': 'center'}),
+    html.H1("World Happiness Report Data Visulization from 2015 to 2021", style={'text-align': 'center','background-color':'#4797c6','color':'white'}),
     html.Div(id='output_container', children=[]),
-    dcc.Dropdown(id="select_year",
-                 options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018},
-                     {"label": "2019", "value": 2019},
-                     {"label": "2020", "value": 2020},
-                     {"label": "2021", "value": 2021}],
-                 multi=False,
-                 value=2015,
-                 style={'width': "40%"}
-                 ),
-    html.Br(),
+    dcc.Slider(
+        id="select_year",
+        min=2015,
+        max=2021,
+        marks={i: 'Year {}'.format(i) for i in range(2015,2022,1)},
+        value=2015,
+    ),
     html.Div(
-        [    dcc.Graph(id='world_map_happiness', style={'width':'60%','float':'left'}),
-            dcc.Graph(id='country_top_ten', style={'width':'40%','float':'left'})]
+        [    dcc.Graph(id='world_map_happiness', style={'width':'55%','float':'left'}),
+            dcc.Graph(id='country_top_ten', style={'width':'45%','float':'left'})]
+    ),
+    html.Div([
+        dcc.RadioItems(
+            options=[
+                {'label': 'Region', 'value': 'Region'},
+                {'label': 'Economy', 'value': 'Economy'},
+            ],
+            value='Region',
+            id="select_region_economy"
+        )
+    ]),
+    html.Div([
+        dcc.Graph(id='pie_region_economy', style={'width': '40%', 'float': 'left'}),
+        ]
+    ),
+    html.Div([
+        dcc.Dropdown(
+            options=[
+                {'label': k, 'value': k} for k in all_country_option
+            ],
+            value=['Denmark'],
+            multi=True,
+            style={'padding-left':'1%','width':'65%'},
+            placeholder="Select a Country or Region to see the change of the rank of happiness during 2015-2021",
+            id='select_country',
+        )
+    ], style={'padding-top':'10%'}),
+    html.Div(
+        [dcc.Graph(id='all_country_compare_score', style={'width': '45%', 'float': 'left'}),
+         dcc.Graph(id='all_country_compare_rank', style={'width': '45%','float': 'left'})],
+
     )
 
 ])
+
+
+#remember to change in the future
+def getValue(year, way):
+    countries = pd.read_csv("2021.csv", nrows=50)
+    values = [0] * 10
+    dict = {'Central and Eastern Europe':0,
+            'Commonwealth of Independent States':1,
+            'Southeast Asia':2,
+            'Middle East and North Africa':3,
+            'Western Europe':4,
+            'Latin America and Caribbean':5,
+            'North America and ANZ':6,
+            'Sub-Saharan Africa':7,
+            'East Asia':8,
+            'South Asia':9}
+
+    if way == 'Region':
+        for index, country in countries.iterrows():
+            print(country['Region'])
+            values[dict.get(country['Region'])] += 1
+    return values
+
+
+
+@app.callback(
+    Output(component_id='pie_region_economy', component_property='figure'),
+    [
+        Input(component_id='select_year', component_property= 'value'),
+        Input(component_id='select_region_economy', component_property= 'value')
+    ]
+)
+def percentRegionEconomy(year, way):
+    labels = ['Central and Eastern Europe',
+              'Commonwealth of Independent States',
+              'Southeast Asia',
+              'Middle East and North Africa',
+              'Western Europe',
+              'Latin America and Caribbean',
+              'North America and ANZ',
+              'Sub-Saharan Africa',
+              'East Asia',
+              'South Asia']
+
+    values = getValue(year, way)
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    return fig
+
+#input : country be chosen
+#output:countries score during 2015-2021 lien chart
+@app.callback(
+    [   Output(component_id='all_country_compare_score', component_property='figure'),
+        Output(component_id='all_country_compare_rank', component_property='figure'),
+    ],
+    [Input(component_id='select_country', component_property='value')]
+)
+def allCountryCompare(choseCountries):
+    scoreFig = go.Figure()
+    rankFig = go.Figure()
+    years = [2015, 2016, 2017, 2018, 2019, 2020, 2021]
+    for country in choseCountries:
+        happiness_score = []
+        for year in years:
+            file = pd.read_csv(str(year)+".csv")
+            for index, data in file.iterrows():
+                if data["Country"] == country:
+                    happiness_score.append(data["Happiness Score"])
+                    print(happiness_score)
+                    break
+        scoreFig.add_trace(go.Scatter(
+            x=years,
+            y=happiness_score,
+            name=country,
+            connectgaps=True
+        ))
+
+    j = 0
+    # Create figure
+    points = []
+    for country in choseCountries:
+        happiness_score = []
+        happiness_rank = []
+        for year in years:
+            file = pd.read_csv(str(year) + ".csv")
+            for index, data in file.iterrows():
+                if data["Country"] == country:
+                    happiness_score.append(data["Happiness Score"])
+                    happiness_rank.append(data["Happiness Rank"])
+                    break
+        j += 1
+        j*= 10
+        points.append(go.Scatter(
+                x=years, y=happiness_rank,
+                mode='markers',
+                marker_size=[np.exp(int(happiness_score[i]))/10 for i in range(len(happiness_score))],
+                name=country
+            ))
+        print([j for i in range(len(happiness_score))])
+        print([int(happiness_score[i]*3) for i in range(len(happiness_score))])
+    rankFig = go.Figure(data=points)
+    scoreFig.update_layout(title_text="Countries score change from 2015 to 2021")
+    rankFig.update_layout(title_text="Countries rank change from 2015 to 2021")
+    return scoreFig, rankFig
+
 
 @app.callback(
     [
@@ -41,15 +173,16 @@ app.layout = html.Div([
     ],
     [Input(component_id='select_year', component_property='value')]
 )
-def getYear(year):
+def yearCompare(year):
     container = "The year chosen by user is: {}".format(year)
     #fileName = str(year)+".csv"
     fileName = "2021.csv"
+    #Get all countries name
     countryMap = pd.read_csv(fileName)
     fig = go.Figure(data=go.Choropleth(
         locations=countryMap['CODE'],
-        z=countryMap['Ladder score'],
-        text=countryMap['Country name'],
+        z=countryMap['Happiness Score'],
+        text=countryMap['Country'],
         colorscale='Blues',
         autocolorscale=False,
         reversescale=False,
@@ -57,9 +190,9 @@ def getYear(year):
         marker_line_width=0.5,
         colorbar_title='Total score',
     ))
-
+    world_title = "{0} World Happiness map".format(year)
     fig.update_layout(
-        title_text='2021 World Happiness',
+        title_text=world_title,
         geo=dict(
             showframe=False,
             showcoastlines=False,
@@ -75,15 +208,37 @@ def getYear(year):
     )
 
     top10CountryName = []
-    top10CountryScore = []
-    countryMap = pd.read_csv("2021.csv", nrows=10)
+    economy = []
+    social_support = []
+    health = []
+    freedom = []
+    generosity = []
+    trust = []
+    dystopia_residual = []
     countries = pd.read_csv("2021.csv", nrows=10)
     for index, country in countries.iterrows():
-        top10CountryName.append(country["Country name"])
-        top10CountryScore.append(country["Ladder score"])
+        top10CountryName.append(country["Country"])
+        economy.append(country["Economy (GDP per Capita)"])
+        social_support.append(country["Social support"])
+        health.append(country["Health (Life Expectancy)"])
+        freedom.append(country["Freedom"])
+        generosity.append(country["Generosity"])
+        trust.append(country["Trust (Government Corruption)"])
+        dystopia_residual.append(country["Dystopia Residual"])
 
-    top10BarChart = go.Figure([go.Bar(x=top10CountryName, y=top10CountryScore)])
 
+    top10BarChart = go.Figure(data=[
+        go.Bar(name='Economy (GDP per Capita)', x=top10CountryName, y=economy),
+        go.Bar(name='Social support', x=top10CountryName, y=social_support),
+        go.Bar(name='Health (Life Expectancy)', x=top10CountryName, y=health),
+        go.Bar(name='Freedom to make life choice', x=top10CountryName, y=freedom),
+        go.Bar(name='Generosity', x=top10CountryName, y=generosity),
+        go.Bar(name='Trust (Government Corruption)', x=top10CountryName, y=trust),
+        go.Bar(name='Dystopia Residual', x=top10CountryName, y=dystopia_residual)
+    ])
+    top10BarChart.update_layout(barmode='stack')
+    title = "The top 10 happiest countries in {0}".format(year)
+    top10BarChart.update_layout(title_text=title)
     return container, fig, top10BarChart
 
 
@@ -92,4 +247,4 @@ def getYear(year):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", debug=True)
